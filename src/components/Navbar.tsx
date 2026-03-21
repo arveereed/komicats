eda34bc77092576448c1307d0a37152dc5cba142
fb.me/arvsdev <arveedurante220@gmail.com>
2026-03-21 16:19:03 +0800

    Modify: make the tailwind works together with shadcn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
+          <div className="flex items-center ">
             <Link
               href="/"
               className="text-xl font-bold text-primary font-mono tracking-wider"
@@ -13,6 +14,9 @@ export default function Navbar() {
               Komicats
             </Link>
           </div>
+          <Link href="/auth/sign-in">
+            <Button variant="default">Sign in</Button>
+          </Link>
 
           {/* <DesktopNavbar />
           <MobileNavbar /> */}
